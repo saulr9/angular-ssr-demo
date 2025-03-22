deb46079470431ffe307fbd03d68badb1f63ae72
Saul Ramos <seramos@cleveritgroup.com>
2025-03-22 14:50:06 -0400

    enhance pagination component; inject ViewportScroller for smooth scrolling and update page change handling

--- src/app/shared/components/pagination/pagination.component.ts
+++ src/app/shared/components/pagination/pagination.component.ts
@@ -1,17 +1,19 @@
+import { ViewportScroller } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
   EventEmitter,
   Input,
   Output,
+  inject,
 } from '@angular/core';
 import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-pagination',
-  imports: [MatButton],
   templateUrl: './pagination.component.html',
   standalone: true,
+  imports: [MatButton],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PaginationComponent {
@@ -19,10 +21,12 @@ export class PaginationComponent {
   @Input() totalPages: number = 1;
   @Output() pageChange = new EventEmitter<number>();
 
+  private viewportScroller = inject(ViewportScroller);
+
   onPageChange(page: number) {
-    console.log('🚀 ~ PaginationComponent ~ onPageChange ~ page:', page);
     if (page >= 1 && page <= this.totalPages) {
       this.pageChange.emit(page);
+      this.viewportScroller.scrollToPosition([0, 0]);
     }
   }
 }
